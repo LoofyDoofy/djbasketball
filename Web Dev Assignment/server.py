17268ece9d5f10fb8179b8781c3d38aa7d9cd22d
LoofyDoofy <88327316+LoofyDoofy@users.noreply.github.com>
2021-08-03 12:14:12 +0800

    Add files via upload

---
+++ Web Dev Assignment/server.py
@@ -0,0 +1,38 @@
+
+import flask
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask("__main__")
+
+@app.route("/")
+def redr():
+    return redirect(url_for('main'))
+
+@app.route("/index.html")
+def main():
+    return render_template(f"index.html")
+
+@app.route("/people.html")
+def people():
+    return render_template(f"people.html")
+
+
+@app.route("/events.html")
+def events():
+    return render_template(f"events.html")
+
+
+@app.route("/askme.html")
+def askme():
+    return render_template(f"askme.html")
+
+
+@app.route("/askmeprocessor.html")
+def askme_processor():
+    return render_template(f"askmeprocessor.html")
+
+if __name__ == "__main__":
+    app.run("0.0.0.0",port=os.getenv('PORT'))
+
+# os.getenv('PORT') change 80
